103933b7b519df2b5764829a6a73272c5108b73b
GarrettGoody <garrettgoody@gmail.com>
2016-01-19 23:01:11 -0800

    added comments

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var first = require('first-match');
-var nub = require('nub');
-var data = require('./data');
+var first = require('first-match'); //to allow easily finding first elements of arrays 
+var nub = require('nub'); //to find all the unique elements of arrays
+var data = require('./data'); //lists all the detail currency code information
 
+
+
+/** 
+ * Returns the first array with the country codes that matches the given code
+ * @param {number} code - a three letter country code, must be valid
+ * @return {array} code  - an array containing all the currency code 
+ * information that matches the country code.
+ */
 var code = function(code) {
 	code = code.toUpperCase();
 
@@ -9,30 +17,53 @@ var code = function(code) {
 		return c.code === code;
 	});
 };
+
+/** 
+ * Returns an array of arrays with the country code information of that country
+ * @param {string} country - a full name of a country 
+ * @return {array} - array containing country code information for all codes within the given country
+ */
 var country = function(country) {
 	country = country.toLowerCase();
 
 	return data.filter(function(c) {
-		return (c.countries || []).indexOf(country) > -1;
+		return (c.countries || []).indexOf(country) > -1; //
 	});
 };
+
+/**
+ * Returns the Currency Code information of the currency with the given code
+ * @param {number} number - a number that represents a currency code, must be valid code
+ * @return {array} - an array containing all the currency code 
+ * information that matches the currency number.
+ */
 var number = function(number) {
 	return first(data, function(c) {
-		return c.number === String(number);
+		return c.number === String(number); //Return the first instance of a matching number in the data file
 	});
 };
+
+/**
+ * Returns a list of all the 3 letter currency codes of the countries in the index.
+ * @return {array} - An array of strings containing 3 letter currency codes.
+ */
 var codes = function() {
 	return data.map(function(c) {
 		return c.code;
 	});
 };
+
+/**
+ * Returns an array of all the countries in the index.
+ * @return {array} - Array of strings with all the countries in the data file.
+ */
 var countries = function() {
 	var m = data.filter(function(c) {
-		return c.countries;
+		return c.countries; //filter out all the countries
 	}).map(function(c) {
 		return c.countries;
 	});
-	return nub(Array.prototype.concat.apply([], m));
+	return nub(Array.prototype.concat.apply([], m)); //concatonate the unique elements of map (countries) into the array
 };
 
 exports.code = code;
