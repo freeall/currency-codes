43daecfd8f10c808133deea2862ad49a8f7955ff
GarrettGoody <garrettgoody@gmail.com>
2016-01-19 23:09:10 -0800

    added docs and examples

---
+++ examples/countries.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var cc = require('../index');
+
+console.log(cc.countries());
---
+++ examples/number.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var cc = require('../index');
+
+console.log('The currency with the Currency Code of 208 is the ' + cc.number(208).currency + '.');
+
+console.log(cc.number(208));
