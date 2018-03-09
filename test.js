6ba131e96351463cd748ef41c494b3b9eea64be0
Chris Jamieson <jamiesoncj@gmail.com>
2018-03-09 15:58:28 +0000

    Fix failing assertion for countries count (245 -> 246)

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-var cc = require('./index');
-var assert = require('assert');
+var cc = require("./index");
+var assert = require("assert");
 
-assert(cc.code('EUR').countries.length === 23);
-assert(cc.number('967').currency === 'Zambian kwacha');
-assert(cc.number(967).currency === 'Zambian kwacha');
-assert(cc.country('colombia').length === 2);
+assert(cc.code("EUR").countries.length === 23);
+assert(cc.number("967").currency === "Zambian kwacha");
+assert(cc.number(967).currency === "Zambian kwacha");
+assert(cc.country("colombia").length === 2);
 assert(cc.codes().length === 179);
-assert(cc.countries().length === 245);
+assert(cc.countries().length === 246);
