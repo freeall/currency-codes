ab344a5f9fe1c30e867926a23946906649611546
Brice Leroy <brice@friendbuy.com>
2018-08-02 16:20:44 -0700

    Add types for typescript

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+declare module "currency-codes" {
+  export interface CurrencyCodeRecord {
+    code: string;
+    number: number;
+    currency: string;
+    countries: string[];
+  }
+
+  export function code(code: string): CurrencyCodeRecord;
+
+  export function country(country: string): CurrencyCodeRecord[];
+
+  export function number(number: number): CurrencyCodeRecord;
+
+  export function codes(): string[];
+
+  export function numbers(): number[];
+
+  export function countries(): string[];
+}
